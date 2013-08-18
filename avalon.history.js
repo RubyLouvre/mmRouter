542ca88d700a8cecd5516e33a145e7b429c33738
RubyLouvre <cheng19840218@gmail.com>
2013-08-18 17:53:55 +0800

    update!

--- avalon.history.js
+++ avalon.history.js
@@ -1,16 +1,13 @@
 define(["avalon"], function(avalon) {
     var History = avalon.History = function() {
-        this.handlers = [];
-
-        this.location = window.location;
         this.location2hash = {}
     };
     var proxy
 
-    // 去最左右两边的斜线
-    var rootStripper = /^\/+|\/+$/g
-    var rstartWithSlant = /^\//
-    var rhashBang = /^\/#(!)?\//
+    var rthimSlant = /^\/+|\/+$/g  // 去最左右两边的斜线
+    var rleftSlant = /^\//         //最左的斜线
+    var rhashBang = /^\/#(!)?\//   //匹配/#/ 或 /#!/
+    var rhashIE = /^[^#]*(#.+)$/
     var anchorElement = document.createElement('a')
 
     History.started = false;
@@ -18,6 +15,10 @@ define(["avalon"], function(avalon) {
         var mode = document.documentMode
         return mode ? mode : window.XMLHttpRequest ? 7 : 6
     })();
+
+    var lastIframeHash = ""
+    var lastDocumentHash = ""
+    var checkerRunning = false;
     var defaults = {basepath: '/', html5Mode: true, hashPrefix: "!", interval: 50}
 
 
@@ -43,7 +44,7 @@ define(["avalon"], function(avalon) {
                 this.html5Mode = false
             }
 
-            anchorElement.href = ('/' + this.options.basepath + '/').replace(rootStripper, '/')
+            anchorElement.href = ('/' + this.options.basepath + '/').replace(rthimSlant, '/')
             var fullpath = !anchorElement.hasAttribute ? anchorElement.getAttribute("href", 4) : anchorElement.href
 
             this.basepath = fullpath.replace(/\/$/, "") //补全路径
@@ -53,11 +54,11 @@ define(["avalon"], function(avalon) {
 
             if (oldIE && !this.html5Mode) {
                 //IE6,7在hash改变时不会产生历史，需要用一个iframe来共享历史
-                avalon.log("IE6,7， 需要注入一个iframe来产生历史")
-                var iframe = avalon.parseHTML('<iframe src="javascript:0" tabindex="-1" style="display:none" />').firstChild
+                var iframe = avalon.parseHTML('<iframe src="#" tabindex="-1" style="display:none" />').firstChild
                 document.body.appendChild(iframe)
                 this.iframe = iframe.contentWindow;
-
+                this.iframe.document.open().close();
+                var startedWithHash = !!proxy.getHash(location.href)
             }
 
             // 支持popstate 就监听popstate
@@ -74,14 +75,45 @@ define(["avalon"], function(avalon) {
                     }
                 }
             }
+            //thanks https://github.com/browserstate/history.js/blob/master/scripts/uncompressed/history.html4.js#L272
+            function checkUrlIE() {
+                if (checkerRunning) {
+                    return false;
+                }
+                checkerRunning = true;
+                var idoc = proxy.iframe.document
+                var documentHash = proxy.location2hash[ location.href ] || ""
+                var iframeHash = proxy.location2hash[ idoc.URL ]
+                if (documentHash !== lastDocumentHash) {//如果是用户点击页面的链接触发
+                    lastDocumentHash = documentHash;
+                    if (iframeHash !== documentHash) {
+                        lastIframeHash = iframeHash = documentHash;
+                        idoc.open().close();//创建历史记录
+                        idoc.location.hash = documentHash.replace(rleftSlant, "")
+                    }
+                    if (avalon.vmodels.xxx) {
+                        avalon.vmodels.xxx.currPath = documentHash
+                    }
+                } else if (iframeHash !== lastIframeHash) {//如果是后退按钮触发hash不一致
+                    lastIframeHash = iframeHash;
+                    if (startedWithHash && iframeHash === '') {
+                        history.go(-1);
+                    } else {
+                        if (iframeHash === "") {
+                            window.location = proxy.basepath + "/"
+                        } else {
+                            location.hash = iframeHash.replace(rleftSlant, "")
+                        }
+                    }
+                }
+                checkerRunning = false;
+            }
             if (this.html5Mode) {
                 this.checkUrl = avalon.bind(window, 'popstate', checkUrl)
-            } else if (this.supportHashChange) {
-                avalon.log("IE 8 9")
+            } else if (this.supportHashChange) {//IE 8, 9与其他不支持push state的浏览器使用hashchange
                 this.checkUrl = avalon.bind(window, 'hashchange', checkUrl)
-            } else {
-                avalon.log("IE 6 7下使用定时器监听URL的变动")
-                this.checkUrl = setInterval(checkUrl, this.options.interval)
+            } else {//IE 6 7下使用定时器监听URL的变动"
+                this.checkUrl = setInterval(checkUrlIE, this.options.interval)
             }
         },
         // 中断URL的监听
@@ -91,10 +123,12 @@ define(["avalon"], function(avalon) {
             clearInterval(this.checkUrl);
             History.started = false;
         },
+        getHash: function(url) {
+            var matches = url.toString().match(rhashIE);
+            return matches ? matches[1] : '';
+        },
         getfullPath: function(url) {
-            var matches = url.toString().match(/^[^#]*(#.+)$/);
-            var hash = matches ? matches[1] : '';
-            return [url.pathname, url.search, hash].join('');
+            return [url.pathname, url.search, this.getHash(url)].join('');
         },
         getLocation: function() {
             return this.getfullPath(window.location);
@@ -112,18 +146,14 @@ define(["avalon"], function(avalon) {
                 }
             } else {
                 if (!this.html5Mode) {//如果支持HTML5 history 新API
-                    path = prefix + path.replace(rstartWithSlant, "");
+                    path = prefix + path.replace(rleftSlant, "");
                 }
             }
             if (path !== this.getLocation()) {
                 this.location2hash[ this.basepath + path ] = path
-                if (this.html5Mode && rstartWithSlant.test(path)) {
+                if (this.html5Mode && rleftSlant.test(path)) {
                     history.pushState({path: path}, window.title, path)
                 } else {
-                    if (this.iframe) {
-                        avalon.log("11111111111111111111111")
-                        this.iframe.document.open().close()
-                    }
                     return (window.location = path)
                 }
             }
@@ -160,6 +190,7 @@ define(["avalon"], function(avalon) {
             window.scrollTo(0, 0);
     }
     //判定A标签的target属性是否指向自身
+    //thanks https://github.com/quirkey/sammy/blob/master/lib/sammy.js#L219
     History.targetIsThisWindow = function targetIsThisWindow(targetWindow) {
         if (!targetWindow || targetWindow === window.name || targetWindow === '_self') {
             return true;
@@ -177,10 +208,9 @@ define(["avalon"], function(avalon) {
     //https://github.com/jashkenas/backbone/blob/master/backbone.js
     proxy = avalon.history = new History;
 
-
     avalon.bind(document, "click", function(event) {
         var defaultPrevented = "defaultPrevented" in event ? event['defaultPrevented'] : event.returnValue === false
-        if (defaultPrevented || event.ctrlKey || event.metaKey || event.which == 2)
+        if (defaultPrevented || event.ctrlKey || event.metaKey || event.which === 2)
             return;
         var target = event.target
         while (target.nodeName !== "A") {
@@ -208,3 +238,4 @@ define(["avalon"], function(avalon) {
 
     return avalon
 })
+
